3e39b90a5f39a3dc39ee316fd720371caf3ad160
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-29 15:55:27 +0100

    aggiunti setters e getters

--- CSharpFunctions/Prodotto.cs
+++ CSharpFunctions/Prodotto.cs
@@ -12,7 +12,7 @@ namespace CSharpShop
         public string nome;
         public string descrizione;
         public int prezzo;
-        private int iva = 22/100;
+        private int iva = 22;
 
         public Prodotto(int codice, string nome, string descrizione, int prezzo)
         {
@@ -24,15 +24,70 @@ namespace CSharpShop
             this.prezzo = prezzo;
         }
      
+        //Getters 
 
+        public int GetCodice()
+        {
+            return this.Codice;
+        }
 
+        public int GetIva()
+        {
+            return this.iva;
+        }
+
+        //SETTERS
+        public void CambiaNome(string nome)
+        {
+            if (nome !="")
+            {
+                this.nome = nome;
+            }
+            else
+            {
+                Console.WriteLine("il nome non può essere vuoto"); ;
+            }
+        }
+        public void CambiaDiscrezione(string descrizione)
+        {
+            if (descrizione != "")
+            {
+                this.descrizione = descrizione;
+            }
+            else
+            {
+                Console.WriteLine("non hai inserito nessuna descrizione");
+            }
+        }
+        public void CambiaPrezzo(int prezzo)
+        {
+            if (prezzo > 0 ) 
+            {
+                this.prezzo = prezzo;
+            }
+            else
+            {
+                Console.WriteLine("il Prezzo non può essere uguale o minore di 0 ");
+            }
+        }
+        public void CambiaIva(int iva)
+        {
+            if (iva > 0)
+            {
+                this.iva = iva;
+            }
+            else
+            {
+                Console.WriteLine("il Prezzo non può essere uguale o minore di 0 ");
+            }
+        }
 
 
 
 
         public int PrezzoCompresoIva()
         {
-            int PIva = prezzo * iva;
+            int PIva = prezzo * (iva / 100);
             int PrezzoFinale = PIva + prezzo;
 
             return PrezzoFinale;
