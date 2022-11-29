49765be27336130377193f45c635f88f8fd8de37
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-29 16:44:47 +0100

    codice testato

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpShop;
+
+Console.WriteLine("inserisci il nome del tu prodotto");
+string NomeProdotto = Console.ReadLine();
+Console.WriteLine("inserisci descrizzione del prodotto");
+string DescrizioneProdotto = Console.ReadLine();
+Console.WriteLine("inserisci il prezzo del prodotto");
+int PrezzoProdotto = Convert.ToInt32(Console.ReadLine());
 
 
+
+
+
+Prodotto InputUtente = new Prodotto(NomeProdotto, DescrizioneProdotto, PrezzoProdotto);
+
+
+InputUtente.StampaDatiProdotto();
