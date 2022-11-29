274f753fef63ded888cefe85e6f7a4c49a163478
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-29 15:22:48 +0100

    creato il mio costruttore

--- CSharpFunctions/Prodotto.cs
+++ CSharpFunctions/Prodotto.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSharpShop
 {
-    internal class Prodotto
+    public class Prodotto
     {
+        private int Codice;
+        public string nome;
+        public string descrizione;
+        private int iva = 22/100;
+
+        public Prodotto (int codice, string nome, string descrizione)
+        {
+            Random rnd = new Random();
+            codice = rnd.Next(10, 20);
+            this.Codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+
+ 
+        }
+
+
     }
 }
