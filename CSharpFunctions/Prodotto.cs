db28d63bce44ca7f5e097c902e40f935771ab0e6
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-29 15:41:23 +0100

    metodo per calcolare iva e altro per stamprae i dati del prodotto

--- CSharpFunctions/Prodotto.cs
+++ CSharpFunctions/Prodotto.cs
@@ -11,17 +11,42 @@ namespace CSharpShop
         private int Codice;
         public string nome;
         public string descrizione;
+        public int prezzo;
         private int iva = 22/100;
 
-        public Prodotto (int codice, string nome, string descrizione)
+        public Prodotto(int codice, string nome, string descrizione, int prezzo)
         {
             Random rnd = new Random();
-            codice = rnd.Next(10, 20);
+            codice = rnd.Next(1,100);
             this.Codice = codice;
             this.nome = nome;
             this.descrizione = descrizione;
+            this.prezzo = prezzo;
+        }
+     
 
- 
+
+
+
+
+
+        public int PrezzoCompresoIva()
+        {
+            int PIva = prezzo * iva;
+            int PrezzoFinale = PIva + prezzo;
+
+            return PrezzoFinale;
+        }
+
+        public void StampaDatiProdotto()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("il Codice del prodotto è :" + Codice);
+            Console.WriteLine("il Nome :" + nome);
+            Console.WriteLine("Descrezione Prodotto :" + descrizione);
+            Console.WriteLine("Prezzo senza iva :" + prezzo + " €");
+            Console.WriteLine("Prezzo compreso iva :" + PrezzoCompresoIva() + " €");
+            Console.WriteLine("-----------------------");
         }
 
 
